6287d1a6e7b51f4b978afe40e9caac992097cb5e
alunny <andrew.lunny@nitobi.com>
2009-11-25 10:12:34 -0800

    change to work with current restful_authentication

--- lib/acts_as_renderer.rb
+++ lib/acts_as_renderer.rb
@@ -1,7 +1,7 @@
 # these must be required because this plugin loads ApplicationController and hooks into it
 require "exception_notifiable"
 require "exception_notifier"
-require File.dirname(RAILS_ROOT) + "/../vendor/plugins/idwccf/lib/authenticated_system"
+require "authenticated_system"
 
 $:.unshift "#{File.dirname(__FILE__)}/lib"
 require 'active_record/acts/renderer'
